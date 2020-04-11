a4d593921641af486873b31fdcde52b33037488b
Yeng Vang <yenghuevang@hotmail.com>
2020-04-10 20:55:08 -0500

    first push

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+require('./controllers/apiRoutes')(app);
+require('./controllers/htmlRoutes')(app);
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`)
+});
